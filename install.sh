8912f555b4ef7b4690e3c41db79b1223ecd94b4f
Andrew Lisowski <lisowski54@gmail.com>
2020-02-03 10:38:23 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install fzf
+(brew --prefix)/opt/fzf/install
 brew install bat
 
 # Install oh-my-zsh
